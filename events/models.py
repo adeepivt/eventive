377d2395fb3e7e74b8cc7b57cf919385e15b2543
adeepivt <adeepivt@gmail.com>
2021-05-21 20:45:01 +0530

    added rating

--- events/models.py
+++ events/models.py
@@ -96,13 +96,24 @@ class Booking(models.Model):
     def __str__(self):
         return f"{self.customer} -- {self.event} -- {self.status}"
 
-
+ratings = (
+    ('1','1'),
+    ('1.5','1.5'),
+    ('2','2'),
+    ('2.5','2.5'),
+    ('3','3'),
+    ('3.5','3.5'),
+    ('4','4'),
+    ('4.5','4.5'),
+    ('5','5'),
+)
 class Review(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='vendor')
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(choices=ratings)
 
     def __str__(self):
         return f'{self.customer}-{self.event.name}'
