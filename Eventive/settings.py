4c16460e3ac54fd28d889266d5de420267a21f58
adeepivt <adeepivt@gmail.com>
2025-04-07 06:48:28 +0530

    Added static files

--- Eventive/settings.py
+++ Eventive/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'Eventive.wsgi.application'
-
+CSRF_TRUSTED_ORIGINS = ['https://eventive.up.railway.app']
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
