f73321cc79620256ea17bb80f26db37c4f0bc03e
adeepivt <adeepivt@gmail.com>
2025-05-20 21:01:37 +0530

    Modified the project structure

---
+++ Eventive/settings/prod.py
@@ -0,0 +1,11 @@
+from .base import *
+
+DEBUG = False
+
+CSRF_TRUSTED_ORIGINS = ['https://eventive.up.railway.app']
+ALLOWED_HOSTS = ['https://eventive.up.railway.app/']
+DATABASES = {
+    'default': env.db() 
+}
+STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
