f8ff9582860dc0115ebb20bd938938791a8b02bd
adeepivt <adeepivt@gmail.com>
2025-08-02 17:15:45 +0530

    Update server conf files

--- Eventive/settings/prod.py
+++ Eventive/settings/prod.py
@@ -2,8 +2,8 @@ from .base import *
 
 DEBUG = False
 
-CSRF_TRUSTED_ORIGINS = ['https://eventive.up.railway.app']
-ALLOWED_HOSTS = ['*','https://eventive.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app/']
+ALLOWED_HOSTS = ['*.railway.app','https://eventive-production.up.railway.app/']
 DATABASES = {
     'default': env.db() 
 }
